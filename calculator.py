0b5a52bfedf5016b1dd35b00cc5c262a694b43c3
Hildur <hildurdav@hotmail.com>
2020-03-29 12:00:40 +0000

    Fimmta test og fjórða aðlagað

--- calculator.py
+++ calculator.py
@@ -5,6 +5,10 @@ class Calc(object):
             return 0
         if input.isdigit():
             return int(input)
-        if len(input.split(",")) > 1:
+        if len(input.split(",")) > 1 and "\n" not in input:
             my_list = [int(i) for i in input.split(",")]
+            return sum(my_list)
+        if "\n" in input:
+            new_input = input.replace("\n", ",")
+            my_list = [int(i) for i in new_input.split(",")]
             return sum(my_list)
--- test_calculator.py
+++ test_calculator.py
@@ -12,10 +12,14 @@ def test_add_twonumbers_returns_sum() -> None:
 def test_add_morenumbers_returns_sum() -> None:
     assert Calc.ADD("1,2,3,4,5") == 15
 
+def test_add_newline_returns_sum() -> None:
+    assert Calc.ADD("1\n2,3") == 6
+
 def main():
     test_add_empty_returns_zero()
     test_add_1_returns_1()
     test_add_twonumbers_returns_sum()
     test_add_morenumbers_returns_sum()
+    test_add_newline_returns_sum()
 
 main()
