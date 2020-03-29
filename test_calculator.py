b76719efe362a842e0181dd5a7a7986a7ef93cb2
Hildur <hildurdav@hotmail.com>
2020-03-29 11:52:00 +0000

    Fjórða test

--- test_calculator.py
+++ test_calculator.py
@@ -9,9 +9,13 @@ def test_add_1_returns_1() -> None:
 def test_add_twonumbers_returns_sum() -> None:
     assert Calc.ADD("1,2") == 3
 
+def test_add_morenumbers_returns_sum() -> None:
+    assert Calc.ADD("1,2,3,4,5") == 15
+
 def main():
     test_add_empty_returns_zero()
     test_add_1_returns_1()
     test_add_twonumbers_returns_sum()
+    test_add_morenumbers_returns_sum()
 
 main()
